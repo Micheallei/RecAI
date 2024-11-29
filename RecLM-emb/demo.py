accc805f451321d5717488656837f7f5dba4e3b0
t-yuxuanlei <t-yuxuanlei@microsoft.com>
2024-11-29 11:21:42 +0000

    update demo

--- RecLM-emb/demo.py
+++ RecLM-emb/demo.py
@@ -15,6 +15,7 @@ from accelerate.utils import set_seed
 
 from preprocess.utils import get_item_text
 from src.huggingface_model_infer import run_model_embedding
+from src.openai_model_infer import run_api_embedding
 import gradio as gr
 
 def gen_retrieval_result(item_embedding_prompt_path, topk, item_embedding_path, user_embedding_path):
@@ -65,9 +66,6 @@ def parse_args():
         "--model_path_or_name", type=str, help="Path to pretrained model or model identifier from huggingface.co/models"
     )
     parser.add_argument(
-        "--topk", type=int, default=10, help=""
-    )
-    parser.add_argument(
         "--seed", type=int, default=2023, help=""
     )
     parser.add_argument(
@@ -107,59 +105,88 @@ if __name__ == '__main__':
     ## get the dir of args.user_embedding_prompt_path
     cache_dir =  os.path.dirname(args.user_embedding_prompt_path)
     item_embedding_prompt_path = os.path.join(cache_dir, 'item_embedding_prompt.jsonl')
-    item_embedding_path = os.path.join(cache_dir, 'item_embedding.pkl')
+    our_model_item_embedding_path = os.path.join(cache_dir, 'item_embedding.pkl')
+    e5_model_item_embedding_path = os.path.join(cache_dir, 'item_embedding_e5.pkl')
+    text_3_large_model_item_embedding_path = os.path.join(cache_dir, 'item_embedding_text_3_large.pkl')
     user_embedding_path = os.path.join(cache_dir, 'user_embedding.pkl')
 
-    if not os.path.exists(item_embedding_path):
+    if not os.path.exists(our_model_item_embedding_path):
         if accelerator.is_main_process:
             os.makedirs(cache_dir, exist_ok=True)
             get_item_text(args.in_meta_data, save_item_prompt_path=item_embedding_prompt_path)
         accelerator.wait_for_everyone()
 
         print("infer item embedding")
-        run_model_embedding(args.model_path_or_name, max_seq_len=args.passage_max_len, batch_size=args.per_device_eval_batch_size, prompt_path=item_embedding_prompt_path, emb_path=item_embedding_path, accelerator=accelerator, args=args, qorp='passage')
+        run_model_embedding(args.model_path_or_name, max_seq_len=args.passage_max_len, batch_size=args.per_device_eval_batch_size, prompt_path=item_embedding_prompt_path, emb_path=our_model_item_embedding_path, accelerator=accelerator, args=args, qorp='passage')
+        run_model_embedding("intfloat/e5-large-v2", max_seq_len=args.passage_max_len, batch_size=args.per_device_eval_batch_size, prompt_path=item_embedding_prompt_path, emb_path=e5_model_item_embedding_path, accelerator=accelerator, args=args, qorp='passage')
+        run_api_embedding("text-embedding-3-large", item_embedding_prompt_path, text_3_large_model_item_embedding_path)
 
-    def user_infer(text):
+    def user_infer(text, model_choice, topk):
         print("infer user embedding")
         with open(args.user_embedding_prompt_path, "w", encoding='utf-8') as f:
             f.write(json.dumps({"text": text})+'\n')
+        
+        if model_choice == "intfloat/e5-large-v2":
+            run_model_embedding(model_choice, max_seq_len=args.query_max_len, batch_size=args.per_device_eval_batch_size, prompt_path=args.user_embedding_prompt_path, emb_path=user_embedding_path, accelerator=accelerator, args=args, qorp='query')
+            if accelerator.is_main_process:
+                base_model_output = gen_retrieval_result(item_embedding_prompt_path, topk, e5_model_item_embedding_path, user_embedding_path)
+            accelerator.wait_for_everyone()
+        elif model_choice == "text-embedding-3-large":
+            run_api_embedding(model_choice, args.user_embedding_prompt_path, user_embedding_path)
+            if accelerator.is_main_process:
+                base_model_output = gen_retrieval_result(item_embedding_prompt_path, topk, text_3_large_model_item_embedding_path, user_embedding_path)
+            accelerator.wait_for_everyone()
+        
         run_model_embedding(args.model_path_or_name, max_seq_len=args.query_max_len, batch_size=args.per_device_eval_batch_size, prompt_path=args.user_embedding_prompt_path, emb_path=user_embedding_path, accelerator=accelerator, args=args, qorp='query')
-
         if accelerator.is_main_process:
-            output_text = gen_retrieval_result(item_embedding_prompt_path, args.topk, item_embedding_path, user_embedding_path)
+            our_model_output = gen_retrieval_result(item_embedding_prompt_path, topk, our_model_item_embedding_path, user_embedding_path)
         accelerator.wait_for_everyone()
-        return output_text
+        return base_model_output[0], base_model_output[1], our_model_output[0], our_model_output[1]
 
     def clear():
-        return "", {}
+        return "", {}, "", {}
 
     with gr.Blocks() as demo:
         gr.HTML("<h1>RecLM-emb Demo</h1>")
-        gr.HTML("<p>This is a demo for paper: <a href='https://dl.acm.org/doi/10.1145/3589335.3651468'>Aligning Language Models for Versatile Text-based Item Retrieval</a>.<br><br><b>Domain</b>: xbox games<br><b>Base model</b>: intfloat/e5-large-v2</p>")
+        gr.HTML("<p>This is a demo for paper: <a href='https://dl.acm.org/doi/10.1145/3589335.3651468'>Aligning Language Models for Versatile Text-based Item Retrieval</a>.<br><br><b>Domain</b>: xbox games</p>")
+        
+        with gr.Row():
+            model_selection = gr.Dropdown(
+                label="Base Model",
+                choices=["intfloat/e5-large-v2", "text-embedding-3-large"],
+                value="intfloat/e5-large-v2"
+            )
+            topk_selection = gr.Number(label="Top K", value=10)
         with gr.Row():
             with gr.Column():
                 input_text = gr.TextArea(label="User Query", placeholder="Type your query here")
                 submit_button = gr.Button("Submit")
                 clear_button = gr.ClearButton(components=[input_text], value="Clear")
+            
             with gr.Column():
-                output_text = gr.TextArea(label="Model Output")   
+                model_1_output = gr.TextArea(label="Base Model Output") 
                 with gr.Accordion("See Details", open=False):
-                    output_text2 = gr.JSON(label="Item Metadata") 
+                    output_json_1 = gr.JSON(label="Item Metadata For Base Model") 
+
+            with gr.Column():
+                model_2_output = gr.TextArea(label="Our Model Output")
+                with gr.Accordion("See Details", open=False):
+                    output_json_2 = gr.JSON(label="Item Metadata For Our Model")
 
         examples = [
-            "I'd like to find some shooting games that are not made for kids and not 2D platformers.",
-            "I'm looking for a sports game, with high quality graphics and soundtrack, released after 2021.",
-            "The Ascend",
-            "Search for games with exploration and science fiction tags, released before 2015.",
-            "Recommend games excluding these elements: Survival Horror and Fighting.",
-            "Search for games with exploration and science fiction tags, developed by M2.",
-            "User: Hi, can you recommend me a game based on what I have played before?\nAssistant: Of course! Please tell me some games you have enjoyed playing.\nUser: I loved Call of Duty Black Ops Cold War, Rust Console Edition, Rocket League, Minecraft, and Fortnite.\nAssistant: Thanks for sharing. What are you looking for in a new game?\nUser: I want a 1st person shooter with a great story, multiplayer, and high-quality soundtrack.",
-            "User: Hey, I'm looking for a new game recommendation. I've played Minecraft, State of Decay 2, Rocket League, For Honor, Middle-earth Shadow of War, Call of Duty Modern Warfare II, and Overwatch 2.\nAssistant: Thanks for sharing your gaming history. What type of game are you looking for now? Any specific genre or features?\nUser: I want something with 3D graphics, realistic art, and a high-quality soundtrack. I also like combat games and multiplayer options.\nAssistant: Got it. Do you prefer a specific setting or any additional features in the game?\nUser: I have a limited budget of $100.",
-        ]
-        gr.Examples(examples=examples, inputs=input_text, outputs=[output_text, output_text2], fn=user_infer, cache_examples=True)
+                    ["I'd like to find some shooting games that are not made for kids and not 2D platformers.", "intfloat/e5-large-v2", 10],
+                    ["I'm looking for a sports game, with high quality graphics and soundtrack, released after 2021.", "intfloat/e5-large-v2", 10],
+                    ["The Ascend", "intfloat/e5-large-v2", 10],
+                    ["Search for games with exploration and science fiction tags, released before 2015.", "text-embedding-3-large", 10],
+                    ["Recommend games excluding these elements: Survival Horror and Fighting.", "text-embedding-3-large", 10],
+                    ["Search for games with exploration and science fiction tags, developed by M2.", "text-embedding-3-large", 10],
+                    # "User: Hi, can you recommend me a game based on what I have played before?\nAssistant: Of course! Please tell me some games you have enjoyed playing.\nUser: I loved Call of Duty Black Ops Cold War, Rust Console Edition, Rocket League, Minecraft, and Fortnite.\nAssistant: Thanks for sharing. What are you looking for in a new game?\nUser: I want a 1st person shooter with a great story, multiplayer, and high-quality soundtrack.",
+                    # "User: Hey, I'm looking for a new game recommendation. I've played Minecraft, State of Decay 2, Rocket League, For Honor, Middle-earth Shadow of War, Call of Duty Modern Warfare II, and Overwatch 2.\nAssistant: Thanks for sharing your gaming history. What type of game are you looking for now? Any specific genre or features?\nUser: I want something with 3D graphics, realistic art, and a high-quality soundtrack. I also like combat games and multiplayer options.\nAssistant: Got it. Do you prefer a specific setting or any additional features in the game?\nUser: I have a limited budget of $100.",
+                ]
+        gr.Examples(examples=examples, inputs=[input_text, model_selection, topk_selection], outputs=[model_1_output, output_json_1, model_2_output, output_json_2], fn=user_infer, cache_examples=True)
 
-        submit_button.click(fn=user_infer, inputs=input_text, outputs=[output_text, output_text2])
-        clear_button.click(fn=clear, outputs=[output_text, output_text2])
+        submit_button.click(fn=user_infer, inputs=[input_text, model_selection, topk_selection], outputs=[model_1_output, output_json_1, model_2_output, output_json_2])
+        clear_button.click(fn=clear, outputs=[model_1_output, output_json_1, model_2_output, output_json_2])
     
     demo.launch(share=True) 
     
--- RecLM-emb/shell/demo.sh
+++ RecLM-emb/shell/demo.sh
@@ -1,17 +1,19 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-RAW_DATA_DIR=$HOME/RecLM-emb/data/steam/raw_data
-EXE_DIR=$HOME/RecLM-emb
+RAW_DATA_DIR=$HOME/RecAI/RecLM-emb/data/steam/raw_data
+EXE_DIR=$HOME/RecAI/RecLM-emb
 
 cd $EXE_DIR
+
+export OPENAI_API_VERSION="xxx";
+export OPENAI_API_BASE="xxx"
 
 accelerate launch --config_file ./shell/infer_case.yaml demo.py \
     --in_meta_data $RAW_DATA_DIR/metadata.json \
     --user_embedding_prompt_path $EXE_DIR/output/demo/user_embedding_prompt.jsonl \
-    --model_path_or_name "intfloat/e5-large-v2" \
-    --topk 5 \
-    --seed 2023 \
+    --model_path_or_name "xxxx" \
+    --seed 2024 \
     --query_max_len 512 \
     --passage_max_len 280 \
     --per_device_eval_batch_size 1 \
